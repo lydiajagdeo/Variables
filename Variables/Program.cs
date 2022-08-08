851083c8f192020fa62b114031d443f5b9c40b84
“Lydia <lydiajagdeo@gmail.com>
2022-08-08 12:11:26 -0400

    Variable UPload

---
+++ Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Zeus";
+            int dogAge = 3;
+            char exclamation = '!';
+            bool SaintBernard = true;
+            double weight = 150.45;
+            decimal drool = 1.4597320m;
+
+            Console.WriteLine($"My dog's name was {dogName}, and he was {dogAge} years old. \n" +
+            $"He was a gentle giant which was so {SaintBernard} and was {weight}lbs{exclamation}\n" +
+            $"He left {drool} inches of slobber everywhere he went, and he is missed."); ;
+        }
+    }
+}
+
